bc537d707572475bb71869dc3203054db62d2361
diegoforlin <diego.forlin@live.com>
2023-09-12 11:52:58 -0300

    Arrumado

--- src/src/Exercicio08Main.java
+++ src/src/Exercicio08Main.java
@@ -8,26 +8,34 @@ public class Exercicio08Main {
 
     public static void main(String[] args) {
 
-            Scanner sc = new Scanner(System.in);
-            int opcao = 0;
+        Scanner sc = new Scanner(System.in);
+        int opcao = 0;
 
-            while (true) {
-                System.out.println("## Escolha uma das opções abaixo ##");
-                System.out.println("Opção 1 - Gestante");
-                System.out.println("Opção 2 - Deficiente");
-                System.out.println("Opção 3 - Idosa/o");
-                System.out.println("Opção 4 - Nenhuma das alternativas");
+        while (true) {
+            System.out.println("## Escolha uma das opções abaixo ##");
+            System.out.println("Opção 1 - Gestante");
+            System.out.println("Opção 2 - Deficiente");
+            System.out.println("Opção 3 - Idosa");
+            System.out.println("Opção 4 - Nenhuma das alternativas");
 
-                System.out.print("Digite aqui sua opção: ");
-                opcao = Integer.parseInt(sc.nextLine());
+            System.out.print("Digite aqui sua opção: ");
+            opcao = Integer.parseInt(sc.nextLine());
 
-                if (opcao == 4) {
-                    System.out.println("Você não tem direito à fila preferencial");
-                    return;
-                } else {
-                    System.out.println("Você tem direito à fila preferencial.");
-                    return;
-                }
+            if (opcao == 4) {
+                System.out.println("Você não tem direito à fila preferencial");
+                return;
+            } else if(opcao == 1) {
+                System.out.println("Você tem direito à fila preferencial.");
+                return;
+            } else if  (opcao == 2) {
+                System.out.println("Você tem direito à fila preferencial.");
+                return;
+            } else if (opcao == 3) {
+                System.out.println("Você tem direito à fila preferencial.");
+                return;
+            } else {
+                System.out.println("Apenas opções de 1 a 4, por favor");
             }
         }
     }
+}
